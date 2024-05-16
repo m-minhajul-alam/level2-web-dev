e402fb2b6072e84688ece455158b77c345c91b47
Minhajul-alam <alam.minhajul@outlook.com>
2024-05-16 07:44:47 +0600

    , , And  Aggregation Stage

---
+++ Aggregation & Indexing/$Bucket, $Sort, And $Limit Aggregation Stage.js
@@ -0,0 +1,34 @@
+db.test.aggregate([
+    // stage 1
+    {
+        $bucket: {
+            groupBy: "$age",
+            boundaries: [20, 40, 60, 80],
+            default: "above 80",
+            output: {
+                count: {
+                    $sum: 1
+                },
+                details: {
+                    $push: "$$ROOT"
+                }
+            }
+        }
+    },
+
+    // stage 2
+    {
+        $sort: { count: -1 }
+    },
+
+    // stage 3
+    {
+        $limit: 2
+    },
+
+    // stage 4
+    {
+        $project: { count: 1 }
+    }
+
+])
